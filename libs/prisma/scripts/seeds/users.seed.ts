0b3a6df9104d24d18f33a696255a1f5203c51015
MarcinParda <marcin98parda@gmail.com>
2024-05-30 22:28:36 +0200

    chore: Update user seed script to use bcrypt for password hashing

--- libs/prisma/scripts/seeds/users.seed.ts
+++ libs/prisma/scripts/seeds/users.seed.ts
@@ -1,16 +1,18 @@
 import { prisma } from '../../src';
 import { faker } from '@faker-js/faker';
+import bcrypt from 'bcrypt';
 
 export async function seedUsers(numUsers: number) {
+  const testPassword = await bcrypt.hash('test', 10);
   const users = Array.from({ length: numUsers - 1 }, () => ({
     username: faker.internet.userName(),
     email: faker.internet.email(),
-    password: faker.internet.password(),
+    password: testPassword,
   }));
   const testUser = {
     username: 'test',
     email: 'test@test.test',
-    password: 'test',
+    password: testPassword,
   };
   users.push(testUser);
 
